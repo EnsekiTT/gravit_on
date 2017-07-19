a1acc99c9148e20dc10a630a5c3e6e70aa26e13d
EnsekiTT <ensekitt@gmail.com>
2017-07-20 00:30:33 +0900

    動き回れるようにした

--- gravit_on/Assets/Scripts/Bullet.cs
+++ gravit_on/Assets/Scripts/Bullet.cs
@@ -3,28 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
-	public GameObject bullet;
-	public Transform muzzle;
-	public float speed;
+	public GameObject planet;
+	public float accelerationScale;
+	public Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// z キーが押された時
-		if(Input.GetMouseButtonDown(0)){
-			// 弾丸の複製
-			GameObject bullets = GameObject.Instantiate(bullet)as GameObject;
-
-			Vector3 force;
-			force = this.gameObject.transform.forward * speed;
-			// Rigidbodyに力を加えて発射
-			bullets.GetComponent<Rigidbody>().AddForce (force);
-			// 弾丸の位置を調整
-			bullets.transform.position = muzzle.position;
-		}
+		var direction = planet.transform.position - transform.position;
+		direction.Normalize ();
+		rb.AddForce (accelerationScale * direction, ForceMode.Acceleration);
 	}
 }
