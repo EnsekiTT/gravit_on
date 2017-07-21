9674a3e6bfe4df760d74ddd761e77415acbc3d93
anoChick <anoinchick@gmail.com>
2017-07-22 00:26:53 +0900

    あのちっく追加

---
+++ gravit_on/Assets/Scripts/AnoChick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnoChick : MonoBehaviour {
+	public GameObject planet;
+	public float accelerationScale;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		//rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var direction = planet.transform.position - transform.position;
+
+		direction.Normalize ();
+		rb.AddForce (accelerationScale * direction, ForceMode.Acceleration);
+	}
+}
