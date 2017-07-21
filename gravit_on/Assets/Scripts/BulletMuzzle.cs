c0d3a17b51cfc9409b84e2a815f9cb3c8b0a75c6
EnsekiTT <ensekitt@gmail.com>
2017-07-21 19:00:57 +0900

    ぷれふぁぶが消えていたのを戻す

---
+++ gravit_on/Assets/Scripts/BulletMuzzle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMuzzle : MonoBehaviour {
+	public GameObject bullet;
+	public Transform muzzle;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// z キーが押された時
+		if(Input.GetMouseButtonDown(0)){
+			// 弾丸の複製
+			GameObject bullets = GameObject.Instantiate(bullet)as GameObject;
+
+			Vector3 force;
+			force = this.gameObject.transform.forward * speed;
+			// Rigidbodyに力を加えて発射
+			bullets.GetComponent<Rigidbody>().AddForce (force);
+			// 弾丸の位置を調整
+			bullets.transform.position = muzzle.position;
+		}
+	}
+}
---
+++ gravit_on/Assets/Scripts/Cube.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+	public float movementSpeed;
+	public GameObject planet;
+	public GameObject body;
+	public float accelerationScale;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var direction = rb.position - planet.transform.position;
+		direction.Normalize ();
+		transform.up = direction;
+		rb.AddForce (accelerationScale * (-direction), ForceMode.Acceleration);
+
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += body.transform.forward * movementSpeed;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position -= body.transform.forward * movementSpeed;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position -= body.transform.right * movementSpeed;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += body.transform.right * movementSpeed;
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rb.velocity = direction * 10;
+		}
+	}
+}
