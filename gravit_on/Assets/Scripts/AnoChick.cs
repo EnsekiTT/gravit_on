5154fdbf20a0c8a51cff3f772b2e7e4baaad146a
anoChick <anoinchick@gmail.com>
2017-07-22 22:47:32 +0900

    あのちっく歩く。

--- gravit_on/Assets/Scripts/AnoChick.cs
+++ gravit_on/Assets/Scripts/AnoChick.cs
@@ -6,17 +6,44 @@ public class AnoChick : MonoBehaviour {
 	public GameObject planet;
 	public float accelerationScale;
 	public Rigidbody rb;
-
+	float directionForward;
+	int collisionCT = 120;
+	int currentCollisionCT;
 	// Use this for initialization
 	void Start () {
-		//rb = GetComponent<Rigidbody> ();
+		directionForward = 0f;
+		currentCollisionCT = 0;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		var direction = planet.transform.position - transform.position;
+		currentCollisionCT--;
+		var direction = transform.position - planet.transform.position;
 
 		direction.Normalize ();
-		rb.AddForce (accelerationScale * direction, ForceMode.Acceleration);
+
+	
+		//transform.up =direction;
+		transform.LookAt(planet.transform.position);
+		transform.Rotate (new Vector3(-90f,0f,0f),Space.Self);
+		transform.Rotate (new Vector3(0f,directionForward,0f),Space.Self);
+
+		rb.AddForce (accelerationScale * -  direction, ForceMode.Acceleration);
+		transform.position += transform.forward * 0.05f;
+
+	}
+
+	void OnCollisionEnter(Collision collision) {
+
+
+
+		if (collision.gameObject.name != "planet" && currentCollisionCT <= 0) {
+			currentCollisionCT = 120;
+			directionForward = Random.Range (0, 360);
+		}
+		if (collision.gameObject.name == "Bullet(Clone)") {
+			GameObject.Destroy (collision.gameObject);
+		}
+
 	}
 }
