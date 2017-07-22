afc9f2521f528041feb466b9a3ed3d568c9aa050
EnsekiTT <ensekitt@gmail.com>
2017-07-22 13:40:07 +0900

    #3 南極付近を一旦禁止エリア化

--- gravit_on/Assets/Scripts/Base.cs
+++ gravit_on/Assets/Scripts/Base.cs
@@ -6,25 +6,19 @@ public class Base : MonoBehaviour {
 	public float movementSpeed;
 	public GameObject planet;
 	public GameObject body;
+	public float azRotateSpeed;
 	public float accelerationScale;
 	public Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		var direction = rb.position - planet.transform.position;
 		direction.Normalize ();
-		var south = 1;
-		if (rb.position.y > 1) {
-			south = 1;
-		} else if(rb.position.y < -1) {
-			south = -1;
-		}
 		transform.up = direction;
 		rb.AddForce (accelerationScale * (-direction), ForceMode.Acceleration);
 
@@ -42,6 +36,9 @@ public class Base : MonoBehaviour {
 		}
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			rb.velocity = direction * 10;
+		}
+		if (transform.position.y < -48 && (-10 < transform.position.x < 10) && (-10 < transform.position.z < 10)){
+			transform.position = new Vector3(transform.position.x, -48, transform.position.z);
 		}
 	}
 }
