572db33ca2776aae64e018dd65a0ca0a9982db4f
EnsekiTT <ensekitt@gmail.com>
2017-08-05 23:38:34 +0900

    #20 とりあえずメニューが出るようにした

---
+++ gravit_on/Assets/Scripts/ButtonExit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonExit : MonoBehaviour {
+
+	public void OnClick(){
+		Debug.Log ("Exit button is clicked!");
+		Application.Quit ();
+	}
+
+}
